5a7b1e2d3456c4a027741a18b373e9e1e93238c2
debrajdas <debraj.das@iths.se>
2022-02-14 22:23:11 +0100

    Navbar changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,7 +29,7 @@ export default {
   justify-content: center;
   min-height: 500px;
   background-color: #0e927d;
-  max-width: 375px;
+  max-width: 457px;
 }
 
 .left {
--- src/views/NavDropDown.vue
+++ src/views/NavDropDown.vue
@@ -1,25 +1,19 @@
 <template>
   <nav>
     <ul>
-      <li><router-link to="/KaffeMeny">Meny</router-link></li>
-      <li><hr /></li>
-      <li><router-link to="/OurCoffee">Vårt Kaffe</router-link></li>
-      <li><hr /></li>
-      <li><router-link to="/OrderHist">Order History</router-link></li>
-      <li><hr /></li>
-      <li><router-link to="/OrderDelivery">Orderstatus</router-link></li>
+      <li><router-link to="/KaffeMeny" exact>Menu</router-link></li>
+      <li><router-link to="/OurCoffee" exact>OurCoffee</router-link></li>      
+      <li><router-link to="/OrderHist" exact>OrderHistory</router-link></li>
+      <li><router-link to="/OrderDelivery" exact>OrderStatus</router-link></li>
     </ul>
   </nav>
 </template>
 
 <style scoped>
 nav {
-  display: flex;
-  justify-content: center;
-  min-height: 700px;
-  background-color: #2f2926;
-  max-width: 375px;
-  align-items: center;
+    background: #444;
+    padding: 14px 0;
+    margin-bottom: 40px;
 }
 
 ul {
@@ -28,9 +22,11 @@ ul {
 }
 
 li {
-  font-size: 1.8rem;
+  font-size: 1.3rem;
   font-family: serif;
   padding: 3px;
+  display: inline-block;
+    margin: 0 10px;
 }
 
 a {
@@ -39,8 +35,15 @@ a {
   line-height: 120%;
 }
 
-hr {
-  width: 50px;
-  border: 1px solid #595451;
+ul li a:hover::before {
+  transform: scale(1.1);
+  box-shadow: 0 0 15px #0d4dd6;
 }
+
+ul li a:hover {
+  color: #eeff00;
+  box-shadow: 0 0 5px #11cedb;
+  text-shadow: 0 0 5px #0ab9ee;
+}
+
 </style>
